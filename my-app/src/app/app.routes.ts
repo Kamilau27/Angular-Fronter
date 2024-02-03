46002c21a36fda697c83d846bb1997287eee5320
Kamilau27 <kamilau27@hotmail.com>
2024-02-02 19:48:45 -0500

    Angular

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { IniciosesionComponent } from './components/iniciosesion/iniciosesion.component';
+import { PrivadaComponent } from './components/privada/privada.component';
 import { NavegacionComponent } from './components/navegacion/navegacion.component';
 import { ContenidoComponent } from './components/contenido/contenido.component';
 import { NoencontradoComponent } from './components/noencontrado/noencontrado.component';
+
 
 export const routes: Routes = [
     { path: 'inicio', component:InicioComponent},
     { path: 'navegacion', component:NavegacionComponent},
     { path: 'contenido', component:ContenidoComponent},
-    { path:'', redirectTo:'inicio', pathMatch:'full'},
+    { path:'', component:InicioComponent},
+    { path: 'iniciosesion', component:IniciosesionComponent},
+    { path: 'privada', component:PrivadaComponent},
+    { path: '', redirectTo:'navegacion', pathMatch:'full'},
     { path:'**', component:NoencontradoComponent, title:'404'}
 ];
---
+++ my-app/src/app/components/privada/privada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privada',
+  standalone: true,
+  imports: [],
+  templateUrl: './privada.component.html',
+  styleUrl: './privada.component.css'
+})
+export class PrivadaComponent {
+
+}
